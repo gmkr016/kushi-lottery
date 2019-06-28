9461bd3f1116e649e4af69cd270f0b8c08d3235e
Susant Paudel <crupee.susant@gmail.com>
2019-06-28 11:31:56 +0545

    dynamically populate lotto category index page ticket generate

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,32 +2,33 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\LotteryCategory as Lcat;
 
 class HomeController extends Controller
 {
 
     public function index()
     {
+        $lcat = Lcat::all();
         $title = "home";
-        return view('frontend.index')->with('title', $title);
+        return view('frontend.index')->with(compact('title', 'lcat'));
     }
 
     public function about()
     {
         $title = "about";
-        return view('frontend.about')->with('title', $title);;
+        return view('frontend.about')->with('title', $title);
     }
 
     public function blog()
     {
         $title = "blog";
-        return view('frontend.blog')->with('title', $title);;
+        return view('frontend.blog')->with('title', $title);
     }
 
     public function contact()
     {
         $title = "contact";
-        return view('frontend.contact')->with('title', $title);;
+        return view('frontend.contact')->with('title', $title);
     }
 }
