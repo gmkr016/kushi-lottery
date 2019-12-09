ff4bd3630a12ab7b3c54bf4514aa0da13cb80bc4
Susant Paudel <crupee.susant@gmail.com>
2019-12-09 15:47:29 +0545

    canvas js changed to chart js

--- resources/views/admin/charts/agentWiseSale.blade.php
+++ resources/views/admin/charts/agentWiseSale.blade.php
@@ -10,55 +10,77 @@ $devHost = $_SERVER['HTTP_HOST']."/dev/public";
     <div id="content">
 
         @include('admin.templates.top-nav')
+
+        {{-- dynamic --}}
+        <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+
+
+        <script type="text/javascript">
+            window.onload = function () {
+                        var host = "{{$host}}" ;
+                        var ctx = document.getElementById('myChart');
+                        var barLabels=[];
+                        var barData=[];
+                        var color=[];
+                        fetch("http://"+host + "/api/agentwisesale") // Call the fetch function passing the url of the API as a parameter
+                            .then( response => response.json() )
+                            .then(json => {
+                                var datas = json;
+                                // pusing datas into label and data
+                                for(var i=0;i<datas.length;i++){ 
+                                    barLabels.push(datas[i].agent); 
+                                    barData.push(datas[i].ticketCount);
+                                    color.push(getRandomColor()); 
+                                } 
+                                    function getRandomColor(){ 
+                                        var letters='0123456789ABCDEF' .split(''); 
+                                        var color='#'; 
+                                        for(var i=0;i<6;i++){ 
+                                            color+=letters[Math.floor(Math.random()*16)]; 
+                                        } 
+                                        return color; 
+                                    } 
+                                    // creating chart chart using labels and data 
+                                    var chart=new Chart(ctx, { 
+                                        // The type of chart we want to create 
+                                        type: 'bar' , 
+                                        // The data for our dataset 
+                                    data: { 
+                                        labels: barLabels, 
+                                        datasets: [{ 
+                                            label: 'Lottery ticket sales of last draw' ,
+                                            backgroundColor: color, 
+                                            borderColor: 'rgb(255, 150, 132)' , 
+                                            fillColor:getRandomColor(), 
+                                            data: barData
+                                        }] 
+                                    }, 
+                                    //Configuration options go here 
+                                    options: {} 
+                                });
+                                
+                                })
+                                .catch(function(error) {
+                                // This is where you run code if the server returns any errors
+                                console.log(error)
+                                }
+                                );
+                                }   
+        </script>
         <div class="container-fluid">
             <div class="card shadow mb-4">
                 <!-- Card Header - Dropdown -->
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                     <h6 class="m-0 font-weight-bold text-primary">Lottery ticket sales of agents of last draw</h6>
                 </div>
-                <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-                <script type="text/javascript">
-                    window.onload = function () {
-                var host = "{{$host}}" ;
-                var fdata;
-                fetch("http://"+host + "/api/agentwisesale") // Call the fetch function passing the url of the API as a parameter
-                    .then( response => response.json() )
-                    .then(json => {
-                        var datas = {
-                            "data": json
-                        };
-                    var dataPoints=[];
-
-                    for(var i=0;i<datas.data.length;i++){ 
-                        dataPoints.push({label:datas.data[i].agent,y:datas.data[i].ticketCount}); } 
-                        var chart=new CanvasJS.Chart("chartContainer", 
-                        { 
-                            animationEnabled: true, 
-                            theme: "light2" , 
-                            zoomEnabled: true,
-                           
-                                    data: [ 
-                                            { 
-                                                type: "column" , 
-                                                dataPoints:dataPoints,
-                                                indexLabel:"Ticket Sold: {y}",
-                                            } 
-                                            ] 
-                        });
-                        chart.render();
-                        })
-                        .catch(function(error) {
-                        // This is where you run code if the server returns any errors
-                        console.log(error)
-                        }
-                        );
-                        }   
-                </script>
                 <div class="card-body">
-                    <div id="chartContainer" style="height: 100%; width: 100%;"></div>
+                    <canvas id="myChart"></canvas>
                 </div>
             </div>
         </div>
+        {{-- dynamic --}}
+
+
     </div>
 </div>
 @endsection
