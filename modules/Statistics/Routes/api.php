ed1968b63c8d1d08fec201794867414b43f7872e
susant <gracysusant@gmail.com>
2024-02-26 12:28:20 +0545

    refactor

--- modules/Statistics/Routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Statistics\Controllers\Api\Agent\StatisticsController;
-
-Route::controller(StatisticsController::class)->group(function () {
-    Route::get('games', 'getCurrentGameStatistics');
-});
--- modules/Statistics/Services/Interfaces/InterfaceStatisticService.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Modules\Statistics\Services\Interfaces;
-
-interface InterfaceStatisticService
-{
-    public function getGamesStatistics(): array;
-}
