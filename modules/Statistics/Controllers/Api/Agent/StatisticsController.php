9db480d10a1763d908366b2e08af6b6a52ba3470
susant <gracysusant@gmail.com>
2024-02-26 12:19:03 +0545

    refactor

--- modules/Statistics/Controllers/Api/Agent/StatisticsController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Modules\Statistics\Controllers\Api\Agent;
-
-use App\Http\Controllers\Controller;
-
-class StatisticsController extends Controller
-{
-}
--- modules/Statistics/Routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Statistics\Controllers\Api\Agent\StatisticsController;
-
-//Route::controller(StatisticsController::class)->group(function () {
-//    Route::get('tickets', 'listTickets');
-//    Route::get('games', 'getCurrentGameStatistics');
-//});
